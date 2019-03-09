14a7355e2bf1474ac283f677acf2578c3b4980dd
Yuki <adohiito@gmail.com>
2019-03-08 23:19:11 -0500

    Revised ver.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let userScore = 0;
 let computerScore = 0;
+
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
@@ -8,17 +9,12 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
-function getComputerChoice(){
-  const choices = ['r','p','s'];
-  const randomNumber = Math.floor(Math.random() * 3);
-  return choices[randomNumber];
-}
-
 function convertToWord(letter){
   if(letter ==  "r") return "Rock";
   if(letter ==  "p") return "Paper";
   return "Scissors";
 }
+
 
 
 function win(userChoice,computerChoice){
@@ -54,6 +50,11 @@ function draw(userChoice,computerChoice){
 
 }
 
+function getComputerChoice(){
+  const choices = ['r','p','s'];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
 
 function game(userChoice){
   const computerChoice = getComputerChoice();
@@ -81,6 +82,8 @@ function game(userChoice){
 
 
 }
+
+
 
 function main(){
   rock_div.addEventListener('click',function(){
